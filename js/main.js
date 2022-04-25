30d4ade88dd5815973175f51fa5b9706ace667de
muhammadsodiq07 <muhammadodiqkoziyev1@gmail.com>
2022-04-25 21:07:40 +0500

    added

--- js/main.js
+++ js/main.js
@@ -44,10 +44,8 @@ formadd.addEventListener("submit", (e) => {
   idMobile.style.border = "1px solid black"
 
   document.querySelectorAll(".line").forEach((item) => {
-    item.style.border = "1px solid black"
+    item.innerText = "";
   });
-  document.querySelector('.add__err').style.display = "none"
-  document.querySelector('.add__err2').style.display = "none"
 });
 
 function showUser(newArr) {
@@ -93,9 +91,16 @@ function editItem(elId) {
       newArr[elId].email = idEmailEdit.value;
       newArr[elId].phone = idMobileEdit.value;
       newArr[elId].department = idSelectEdit.value;
-
       showUser(newArr);
 
+
+      idNameEdit.style.border = "1px solid black"
+      idEmailEdit.style.border = "1px solid black"
+      idMobileEdit.style.border = "1px solid black"
+
+      document.querySelectorAll(".EditM").forEach((item) => {
+        item.innerText = "";
+      });
       count++;
     }
   });
@@ -127,6 +132,7 @@ searchUser.addEventListener("keyup", () => {
     }
   })
 });
+
 
 
 idMobile.addEventListener("keyup", () => {
@@ -171,18 +177,18 @@ idEmail.addEventListener("keyup", () => {
 });
 
 
-idEmail.addEventListener("keyup", () => {
-  let text = document.querySelector(".add__err2");
+idName.addEventListener("keyup", () => {
+  let text = document.querySelector(".add__err1");
   try {
-    let elName = idEmail.value;
-    if (!/^\S+@\S+\.\S+$/.test(idEmail.value)) {
-      idEmail.style.outline = "none";
-      idEmail.style.border = "2px solid red";
+    let elName = idName.value;
+    if (elName == "") {
+      idName.style.outline = "none";
+      idName.style.border = "2px solid red";
       text.style.color = "red";
-      throw "Enter a valid email";
+      throw "Enter a valid name";
     } else {
-      idEmail.style.outline = "none";
-      idEmail.style.border = "2px solid green";
+      idName.style.outline = "none";
+      idName.style.border = "2px solid green";
       text.style.color = "green";
       throw "Success";
     }
@@ -192,6 +198,171 @@ idEmail.addEventListener("keyup", () => {
 });
 
 
+idMobileEdit.addEventListener("keyup", () => {
+  let text = document.querySelector(".add__erreD2");
+  try {
+    let elName = idMobileEdit.value;
+    if (!Number(elName) || elName.length < 12) {
+      idMobileEdit.style.outline = "none";
+      idMobileEdit.style.border = "2px solid red";
+      text.style.color = "red";
+      throw "Enter a valid mobile";
+    } else {
+      idMobileEdit.style.outline = "none";
+      idMobileEdit.style.border = "2px solid green";
+      text.style.color = "green";
+      throw "Success";
+    }
+  } catch (e) {
+    text.innerText = e;
+  }
+});
+
+
+idEmailEdit.addEventListener("keyup", () => {
+  let text = document.querySelector(".add__erreD1");
+  try {
+    let elName = idEmailEdit.value;
+    if (!/^\S+@\S+\.\S+$/.test(idEmailEdit.value)) {
+      idEmailEdit.style.outline = "none";
+      idEmailEdit.style.border = "2px solid red";
+      text.style.color = "red";
+      throw "Enter a valid email";
+    } else {
+      idEmailEdit.style.outline = "none";
+      idEmailEdit.style.border = "2px solid green";
+      text.style.color = "green";
+      throw "Success";
+    }
+  } catch (e) {
+    text.innerText = e;
+  }
+});
+
+
+idNameEdit.addEventListener("keyup", () => {
+  let text = document.querySelector(".add__erreD");
+  try {
+    let elName = idNameEdit.value;
+    if (elName == "") {
+      idNameEdit.style.outline = "none";
+      idNameEdit.style.border = "2px solid red";
+      text.style.color = "red";
+      throw "Enter a valid name";
+    } else {
+      idNameEdit.style.outline = "none";
+      idNameEdit.style.border = "2px solid green";
+      text.style.color = "green";
+      throw "Success";
+    }
+  } catch (e) {
+    text.innerText = e;
+  }
+});
+
+
+
+
+let elHeadBtns = document.querySelectorAll(".head-btn");
+
+let count = 0;
+let emailCount = 0;
+let phoneCount = 0;
+let departmentCount = 0;
+
+elHeadBtns.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    if (index == 0) {
+      if (count == 0) {
+        newArr.sort(function (a, b) {
+          if (a.name.toLowerCase() < b.name.toLowerCase()) {
+            return -1;
+          } else if (a.name.toLowerCase() > b.name.toLowerCase()) {
+            return 1;
+          }
+        })
+        showUser(newArr);
+        count++;
+      } else {
+        newArr.sort(function (a, b) {
+          if (a.name.toLowerCase() > b.name.toLowerCase()) {
+            return -1;
+          } else if (a.name.toLowerCase() < b.name.toLowerCase()) {
+            return 1;
+          }
+        })
+        showUser(newArr);
+        count = 0;
+      }
+    } else if (index == 1) {
+      if (emailCount == 0) {
+        newArr.sort(function (a, b) {
+          if (a.email.toLowerCase() < b.email.toLowerCase()) {
+            return -1;
+          } else if (a.email.toLowerCase() > b.email.toLowerCase()) {
+            return 1;
+          }
+        })
+        showUser(newArr);
+        emailCount++;
+      } else {
+        newArr.sort(function (a, b) {
+          if (a.email.toLowerCase() > b.email.toLowerCase()) {
+            return -1;
+          } else if (a.email.toLowerCase() < b.email.toLowerCase()) {
+            return 1;
+          }
+        })
+        showUser(newArr);
+        emailCount = 0;
+      }
+    } else if (index == 2) {
+      if (phoneCount == 0) {
+        newArr.sort(function (a, b) {
+          if (a.phone.toLowerCase() < b.phone.toLowerCase()) {
+            return -1;
+          } else if (a.phone.toLowerCase() > b.phone.toLowerCase()) {
+            return 1;
+          }
+        })
+        showUser(newArr);
+        phoneCount++;
+      } else {
+        newArr.sort(function (a, b) {
+          if (a.phone.toLowerCase() > b.phone.toLowerCase()) {
+            return -1;
+          } else if (a.phone.toLowerCase() < b.phone.toLowerCase()) {
+            return 1;
+          }
+        })
+        showUser(newArr);
+        phoneCount = 0;
+      }
+    } else if (index == 3) {
+      if (departmentCount == 0) {
+        newArr.sort(function (a, b) {
+          if (a.department.toLowerCase() < b.department.toLowerCase()) {
+            return -1;
+          } else if (a.department.toLowerCase() > b.department.toLowerCase()) {
+            return 1;
+          }
+        })
+        showUser(newArr);
+        departmentCount++;
+      } else {
+        newArr.sort(function (a, b) {
+          if (a.department.toLowerCase() > b.department.toLowerCase()) {
+            return -1;
+          } else if (a.department.toLowerCase() < b.department.toLowerCase()) {
+            return 1;
+          }
+        })
+        showUser(newArr);
+        departmentCount = 0;
+      }
+    }
+  });
+});
 
 
 
